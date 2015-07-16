d267d8d1705a4b067bdbd6604283d412afa187fa
Jack Callister <jack.callister@quillcontent.com>
2015-07-16 09:18:05 +0100

    Switch to react router 0.13.3

--- client/app.jsx
+++ client/app.jsx
@@ -1,25 +1,17 @@
 'use strict';
 
 import React from 'react';
-import { Router } from 'react-router';
+import Router from 'react-router';
 import { createRedux } from 'redux';
 import { Provider } from 'redux/react';
-import BrowserHistory from 'react-router/lib/BrowserHistory';
 import * as stores from '../src/stores/index';
 import routes from '../src/routes';
 
-const history = new BrowserHistory();
 const redux = createRedux(stores);
 
 document.addEventListener('DOMContentLoaded', () =>
-  React.render(
-    <Provider redux={redux}>
-      {() =>
-        <Router children={routes}
-                history={history} />
-      }
-    </Provider>,
-    document.getElementById('app')
-  )
-);
 
+  Router.run(routes, Router.HistoryLocation, function(Handler, route) {
+    React.render(<Handler redux={redux} />, document.getElementById('app'));
+  })
+);
--- server/utils/render.js
+++ server/utils/render.js
@@ -1,12 +1,6 @@
 'use strict';
 
-import Location from 'react-router/lib/Location';
 import React from 'react';
-import { Router } from 'react-router';
-import { createRedux } from 'redux';
-import { Provider } from 'redux/react';
-import * as stores from '../../src/stores/index';
-import routes from '../../src/routes';
 
 export default (req, res, data) => {
   res.render('index');
